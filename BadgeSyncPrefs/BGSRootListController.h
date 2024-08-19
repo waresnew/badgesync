862fc7b0b9160cec7b0795a6ab7329e931c75b4a
newwares <45801973+waresnew@users.noreply.github.com>
2024-08-19 16:52:41 -0400

    quotes

--- BadgeSyncPrefs/BGSRootListController.h
+++ BadgeSyncPrefs/BGSRootListController.h
@@ -1,7 +1,7 @@
 #import <Preferences/PSListController.h>
 #import <CepheiPrefs/HBRootListController.h>
 #import <Cephei/HBRespringController.h>
-#import <AltList/AltList.h>
+#import "AltList/AltList.h"
 
 @interface BGSRootListController : HBRootListController
 
