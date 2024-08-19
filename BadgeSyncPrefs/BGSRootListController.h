72a34f64a7356e579003fb1ac9a0063d3cacf524
newwares <45801973+waresnew@users.noreply.github.com>
2024-08-19 18:31:46 -0400

    copy headers

--- BadgeSyncPrefs/BGSRootListController.h
+++ BadgeSyncPrefs/BGSRootListController.h
@@ -1,7 +1,7 @@
 #import <Preferences/PSListController.h>
 #import <CepheiPrefs/HBListController.h>
 #import <Cephei/HBRespringController.h>
-#import "AltList/AltList.h"
+#import <AltList/AltList.h>
 
 @interface BGSRootListController : HBListController
 
